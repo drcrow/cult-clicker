a2d4efbacf59245b20479426cb74cd94947dd5e7
Agustin Fiori <afiori@clarika.com.ar>
2020-12-27 03:05:23 -0300

    Basic Pray, Recruit, Collect mechanic

---
+++ scripts.js
@@ -0,0 +1,79 @@
+var gameStats = {
+    "fait": {
+        "points": 0,
+        "increment": 0
+    },
+    "members": {
+        "points": 0,
+        "increment": 0,
+        "cost": {
+            "stat": "fait",
+            "amount": 10,
+            "modifier": 1.1
+        }
+    },
+    "money": {
+        "points": 0,
+        "increment": 0,
+        "cost": {
+            "stat": "members",
+            "amount": 1,
+            "modifier": 1.1
+        }
+    },
+};
+
+function updateLabels(){
+    $('#label-fait-pts').text(gameStats.fait.points);
+    $('#label-fait-inc').text(gameStats.fait.increment);
+
+    $('#label-members-pts').text(gameStats.members.points);
+    $('#label-members-inc').text(gameStats.members.increment);
+    $('#label-members-cost').text(gameStats.members.cost.amount);
+
+    $('#label-money-pts').text(gameStats.money.points);
+    $('#label-money-inc').text(gameStats.money.increment);
+}
+
+var intervalID = window.setInterval(updateLabels, 1000);
+
+function pray(){
+    gameStats.fait.points = gameStats.fait.points + 1;
+    updateLabels();
+}
+
+function recruit(){
+    if(checkCost('members')){
+        spendCost('members');
+        gameStats.members.points = gameStats.members.points + 1;
+        updateLabels();
+    }
+}
+
+function collect(){
+    if(checkCost('money')){
+        spendCost('money');
+        gameStats.money.points = gameStats.money.points + 10;
+        updateLabels();
+    }
+}
+
+function checkCost(stat){
+    var costStat = gameStats[stat].cost.stat;
+    var costAmount = gameStats[stat].cost.amount;
+
+    if(gameStats[costStat].points >= costAmount){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function spendCost(stat){
+    var costStat = gameStats[stat].cost.stat;
+    var costAmount = gameStats[stat].cost.amount;
+    var newCostAmount = Math.round(gameStats[stat].cost.amount * gameStats[stat].cost.modifier);
+
+    gameStats[costStat].points = gameStats[costStat].points - costAmount;
+    gameStats[stat].cost.amount = newCostAmount;
+}
